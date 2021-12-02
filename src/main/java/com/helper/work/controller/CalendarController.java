f93a85765242df9d6f50ff3a8df36db21f8b6624
Fede Failla <fedefailla18@gmail.com>
2021-12-02 11:50:20 -0300

    added React architecture and Spring Hal

--- src/main/java/com/helper/work/controller/CalendarController.java
+++ src/main/java/com/helper/work/controller/CalendarController.java
@@ -1,16 +1,10 @@
 package com.helper.work.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class CalendarController {
-
-    @RequestMapping("/")
-    String home() {
-        return "Hello World!";
-    }
 
     @GetMapping("/hello")
     public String getCalendar() {
